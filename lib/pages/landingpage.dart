b31e897cabba3bba5089f02578fe0aba8d22a42c
Aakash574 <panwara925@gmail.com>
2023-03-04 15:45:30 +0530

    Login SignUp Page

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:hackathon_project/const.dart';
-import 'package:hackathon_project/pages/loignSignupPage.dart';
+import 'package:hackathon_project/pages/loginPage.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
@@ -14,42 +14,50 @@ class LandingPage extends StatelessWidget {
         height: 740,
         color: Colors.white,
         // alignment: Alignment.center,
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image(
-              image: NetworkImage(
-                  "https://media.istockphoto.com/id/1210803911/vector/people-working-together-hackathon-vector-flat-illustration-programmers-work-with-data.jpg?s=612x612&w=0&k=20&c=yoTjL26kAyCebnpuA-DelwoSOf1EBmazkl8HKragWTw="),
-            ),
-            SizedBox(height: 20),
-            Container(
-              // color: Colors.red,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const LoginSignUpPage()),
-                  );
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(
-                    MyColors.primaryColor,
-                  ),
-                  padding: MaterialStatePropertyAll(
-                    EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Spacer(),
+                Image(
+                  image: NetworkImage(
+                      "https://media.licdn.com/dms/image/C4E0BAQEGfke4fZtFWw/company-logo_200_200/0/1676560286028?e=2147483647&v=beta&t=X8Wk6YuWFevovy9Js96ZtDVyu7s0tHS8wwGZnTRI1ko"),
+                ),
+                SizedBox(height: 20),
+                Text("Text"),
+                SizedBox(height: 20),
+                Container(
+                  // color: Colors.red,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 0, 20, 131),
+                      ),
+                      padding: MaterialStatePropertyAll(
+                        EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                      ),
+                    ),
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                        color: MyColors.textColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
                   ),
                 ),
-                child: Text(
-                  "Get Started",
-                  style: TextStyle(
-                    color: MyColors.textColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-              ),
+              ],
             ),
           ],
         ),
